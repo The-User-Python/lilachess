5f176e45e7c60dfd0779c3448c70e4b69ecf50b5
Adventuresofbot <102851086+Adventuresofbot@users.noreply.github.com>
2022-05-09 16:52:38 +0530

    Create close.scala

---
+++ app/views/account/close.scala
@@ -0,0 +1,41 @@
+package views.html
+package account
+
+import lila.api.Context
+import lila.app.templating.Environment._
+import lila.app.ui.ScalatagsTemplate._
+
+import controllers.routes
+
+object close {
+
+  import trans.settings._
+
+  def apply(u: lila.user.User, form: play.api.data.Form[_], managed: Boolean)(implicit ctx: Context) =
+    account.layout(
+      title = s"${u.username} - ${closeAccount.txt()}",
+      active = "close"
+    ) {
+      div(cls := "account box box-pad")(
+        h1(dataIcon := "", cls := "text")(closeAccount()),
+        if (managed)
+          p("Your account is managed, and cannot be closed.")
+        else
+          postForm(cls := "form3", action := routes.Account.closeConfirm)(
+            div(cls := "form-group")(closeAccountExplanation()),
+            div(cls := "form-group")(cantOpenSimilarAccount()),
+            form3.passwordModified(form("passwd"), trans.password())(autofocus, autocomplete := "off"),
+            form3.actions(
+              frag(
+                a(href := routes.User.show(u.username))(changedMindDoNotCloseAccount()),
+                form3.submit(
+                  closeAccount(),
+                  icon = "".some,
+                  confirm = closingIsDefinitive.txt().some
+                )(cls := "button-red")
+              )
+            )
+          )
+      )
+    }
+}
