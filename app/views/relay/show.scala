f1aa61c86609cdee8c0e1f3daaba1154a11c3260
Adventuresofbot <102851086+Adventuresofbot@users.noreply.github.com>
2022-05-10 15:36:52 +0530

    Add files via upload

---
+++ app/views/relay/show.scala
@@ -0,0 +1,69 @@
+package views.html
+package relay
+
+import play.api.libs.json.Json
+
+import lila.api.Context
+import lila.app.templating.Environment._
+import lila.app.ui.ScalatagsTemplate._
+import lila.common.String.html.safeJsonValue
+
+import controllers.routes
+
+object show {
+
+  def apply(
+      rt: lila.relay.RelayRound.WithTourAndStudy,
+      data: lila.relay.JsonView.JsData,
+      chatOption: Option[lila.chat.UserChat.Mine],
+      socketVersion: lila.socket.Socket.SocketVersion,
+      streamers: List[lila.user.User.ID]
+  )(implicit ctx: Context) =
+    views.html.base.layout(
+      title = rt.fullName,
+      moreCss = cssTag("analyse.relay"),
+      moreJs = frag(
+        analyseTag,
+        analyseNvuiTag,
+        embedJsUnsafe(s"""lichess.relay=${safeJsonValue(
+            Json.obj(
+              "relay"    -> data.relay,
+              "study"    -> data.study.add("admin" -> isGranted(_.StudyAdmin)),
+              "data"     -> data.analysis,
+              "i18n"     -> bits.jsI18n,
+              "tagTypes" -> lila.study.PgnTags.typesToString,
+              "userId"   -> ctx.userId,
+              "chat" -> chatOption.map(c =>
+                chat.json(
+                  c.chat,
+                  name = trans.chatRoom.txt(),
+                  timeout = c.timeout,
+                  writeable = ctx.userId.??(rt.study.canChat),
+                  public = true,
+                  resourceId = lila.chat.Chat.ResourceId(s"relay/${c.chat.id}"),
+                  localMod = ctx.userId.??(rt.study.canContribute)
+                )
+              ),
+              "explorer"      -> views.html.board.bits.explorerConfig,
+              "socketUrl"     -> views.html.study.show.socketUrl(rt.study.id.value),
+              "socketVersion" -> socketVersion.value
+            )
+          )}""")
+      ),
+      chessground = false,
+      zoomable = true,
+      csp = defaultCsp.withWebAssembly.withWikiBooks.some,
+      openGraph = lila.app.ui
+        .OpenGraph(
+          title = rt.fullName,
+          url = s"$netBaseUrl${rt.path}",
+          description = shorten(rt.tour.description, 152)
+        )
+        .some
+    )(
+      frag(
+        main(cls := "analyse"),
+        views.html.study.bits.streamers(streamers)
+      )
+    )
+}
